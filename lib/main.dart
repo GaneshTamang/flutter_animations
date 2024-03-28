57b94078c5dfbf822c33a388e39332aaef12c630
ganeshtamang <tamangganesh@hotmail.com>
2024-03-28 19:39:02 +0545

    added seperated implicit and Explicit animations type

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/screens/main_screen.dart';
+import 'package:flutter_animations/animation_types_example/implicit_aniamtions_example5.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
       ),
-      home: const MainScreen(),
+      home: const MYIMplicitExample5(),
     );
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/animtions_types_exmples/hero_animation_hero_example4.dart';
-import 'package:flutter_animations/animtions_types_exmples/three_d_animations_example3.dart';
-import 'package:flutter_animations/animtions_types_exmples/animated_container_example0.dart';
-import 'package:flutter_animations/animtions_types_exmples/example2.dart';
-import 'package:flutter_animations/animtions_types_exmples/test_transformation.dart';
-import 'package:flutter_animations/animtions_types_exmples/transform_animation_example1.dart';
+import 'package:flutter_animations/animation_types_example/hero_animation_hero_example4.dart';
+import 'package:flutter_animations/animation_types_example/three_d_animations_example3.dart';
+import 'package:flutter_animations/animation_types_example/animated_container_example0.dart';
+import 'package:flutter_animations/animation_types_example/example2.dart';
+import 'package:flutter_animations/animation_types_example/test_transformation.dart';
+import 'package:flutter_animations/animation_types_example/transform_animation_example1.dart';
 import 'package:flutter_animations/custom_widgets/custom_naviagtion_button.dart';
 
 class MainScreen extends StatelessWidget {
@@ -12,12 +12,35 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(children: const [
-        Column(
+    return const Scaffold(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            MCMaterialButton(
+              buttonNAme: "Explcit Animations",
+              pageClass: MyExplicitAniMations(),
+            ),
+            MCMaterialButton(
+              buttonNAme: 'ImplicitAnimations',
+              pageClass: MyImplicitAnimations(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyExplicitAniMations extends StatelessWidget {
+  const MyExplicitAniMations({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: const [
             MCMaterialButton(
               buttonNAme: "MyTestTansformations",
               pageClass: TestTransformation(),
@@ -44,7 +67,27 @@ class MainScreen extends StatelessWidget {
             ),
           ],
         ),
-      ]),
+      ),
+    );
+  }
+}
+
+class MyImplicitAnimations extends StatelessWidget {
+  const MyImplicitAnimations({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          MCMaterialButton(
+            buttonNAme: 'Explicit customAnimation ',
+            pageClass: MyImplicitAnimations(),
+          ),
+        ],
+      ),
     );
   }
 }
